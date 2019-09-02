cd695c9e954e475e146c32f2e9c424333a136fbf
BradleyBrown19 <bradley19brown@gmail.com>
2019-09-02 15:06:15 -0400

    added animation and links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { fadeInOnEnterAnimation, fadeInUpOnEnterAnimation, shakeAnimation, swingAnimation, flipAnimation, wobbleAnimation, jelloAnimation, rotateAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    fadeInOnEnterAnimation(),
+    fadeInUpOnEnterAnimation({ anchor: 'enter', duration: 4000, delay: 500, translate: '600px' }),
+    shakeAnimation(),
+    swingAnimation(),
+    flipAnimation(),
+    wobbleAnimation(),
+    jelloAnimation(),
+    rotateAnimation()
+  ],
 })
 export class AppComponent {
   title = 'personal-website';
+  animationState = false;
+  animationWithState;
+  animationLI;
+  animationR;
+  animationE;
+  animationG;
+  animationLogo;
+
+
+  animate(element) {
+    console.log('ANIMATE')
+    if (element == 'li') {
+      this.animationLI = false;
+      setTimeout(() => {
+        this.animationLI = true;
+      }, 10);
+    } else if (element == 'resume') {
+      this.animationR = false;
+      setTimeout(() => {
+        this.animationR = true;
+      }, 10);
+    } else if (element == 'email') {
+      this.animationE = false;
+      setTimeout(() => {
+        this.animationE = true;
+      }, 10);
+    } else if (element == 'git') {
+      this.animationG = false;
+      setTimeout(() => {
+        this.animationG = true;
+      }, 10);
+    } else if (element == 'logo') {
+      this.animationLogo = false;
+      setTimeout(() => {
+        this.animationLogo = true;
+      }, 60);
+    }
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbCardModule, NbSidebarModule, NbButtonComponent, NbButtonModule } from '@nebular/theme';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule} from '@angular/material'
 
 @NgModule({
   declarations: [
@@ -10,7 +14,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbCardModule,
+    NbSidebarModule,
+    NbButtonModule,
+    MatButtonModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
